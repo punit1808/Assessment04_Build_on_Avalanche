aae53a8a1142c93a5a94e921b18dcc5cba5a4db6
punit1808 <112548386+punit1808@users.noreply.github.com>
2024-06-17 10:23:27 +0530

    Points.sol

--- Points.sol
+++ Points.sol
@@ -9,6 +9,7 @@ contract MY_ERC_Token {
     address private Owner;
 
     mapping(address => uint) private balance_atAddress;
+    mapping(address => uint) private redeemedItems_atAddress;
     mapping(address => bool)  private isExist;
 
     event Transfer(address indexed from, address indexed to, uint value);
@@ -30,13 +31,13 @@ contract MY_ERC_Token {
 
     function validateAccount(address validAcc) public {
         isExist[validAcc] = true;
-        emit ValidateAccount(validAcc);  // Emit event
+        emit ValidateAccount(validAcc); 
     }
 
     function mint(address mintAt, uint mintValue) public ownerAccess {
         balance_atAddress[mintAt] += mintValue;
         tokenSupply += mintValue;
-        emit Mint(mintAt, mintValue);  // Emit event
+        emit Mint(mintAt, mintValue);
     }
 
     function burn(address burnFrom) public {
@@ -44,16 +45,19 @@ contract MY_ERC_Token {
         uint burnAmount = balance_atAddress[burnFrom];
         tokenSupply -= burnAmount;
         balance_atAddress[burnFrom] = 0;
-        emit Burn(burnFrom, burnAmount);  // Emit event
+        emit Burn(burnFrom, burnAmount); 
     }
 
-    function redeem(address redeemFrom, uint redeemValue) public {
+    function redeem(address redeemFrom) public returns (uint) {
+        uint redeemValue = 500;
         require(isExist[redeemFrom], "Account doesn't exist.");
         require(balance_atAddress[redeemFrom] >= redeemValue, "Insufficient Balance Balance");
         tokenSupply -= redeemValue;
         balance_atAddress[redeemFrom] -= redeemValue;
-        emit Redeem(redeemFrom, redeemValue); // Emit event
 
+        redeemedItems_atAddress[redeemFrom] += 1; // One redeemed items added at address 
+        emit Redeem(redeemFrom, redeemValue);
+        return redeemedItems_atAddress[redeemFrom]; // Returning Total Redeemed Items Available at address
     }
 
     function totalSupply() public view returns (uint) {
@@ -71,7 +75,6 @@ contract MY_ERC_Token {
         
         balance_atAddress[msg.sender] -= transferValue;
         balance_atAddress[transferAt] += transferValue;
-        emit Transfer(msg.sender, transferAt, transferValue);  // Emit event
+        emit Transfer(msg.sender, transferAt, transferValue); 
     }
 }
-
