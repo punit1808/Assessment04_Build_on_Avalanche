7bcc1e78ce6e7fefc4ae0a22158d31533a32f6d2
punit1808 <112548386+punit1808@users.noreply.github.com>
2024-06-21 10:34:14 +0530

    Points.sol

--- Points.sol
+++ Points.sol
@@ -5,18 +5,19 @@ contract MY_ERC_Token {
 
     string public tokenName = "DEGEN";
     string public tokenSymbol = "DGN";
-    uint private  tokenSupply = 10000;
+    uint private tokenSupply = 10000;
     address private Owner;
 
     mapping(address => uint) private balance_atAddress;
-    mapping(address => uint) private redeemedItems_atAddress;
-    mapping(address => bool)  private isExist;
+    mapping(address => uint) private redeemedItems_atAddress; // mapping for redeemed Items count
+    mapping(address => string[]) private redeemedItemNames_atAddress; // mapping for redeemed Item names
+    mapping(address => bool) private isExist;
 
     event Transfer(address indexed from, address indexed to, uint value);
     event Mint(address indexed to, uint value);
     event Burn(address indexed from, uint value);
     event ValidateAccount(address indexed account);
-    event Redeem(address indexed from, uint value);
+    event Redeem(address indexed from, uint value, string itemName);
 
     constructor(){
         Owner = msg.sender;
@@ -48,16 +49,23 @@ contract MY_ERC_Token {
         emit Burn(burnFrom, burnAmount); 
     }
 
-    function redeem(address redeemFrom) public returns (uint) {
+    function redeem(address redeemFrom, string memory itemName) public returns (uint) {
         uint redeemValue = 500;
         require(isExist[redeemFrom], "Account doesn't exist.");
-        require(balance_atAddress[redeemFrom] >= redeemValue, "Insufficient Balance Balance");
+        require(balance_atAddress[redeemFrom] >= redeemValue, "Insufficient Balance");
+
         tokenSupply -= redeemValue;
         balance_atAddress[redeemFrom] -= redeemValue;
 
-        redeemedItems_atAddress[redeemFrom] += 1; // One redeemed items added at address 
-        emit Redeem(redeemFrom, redeemValue);
-        return redeemedItems_atAddress[redeemFrom]; // Returning Total Redeemed Items Available at address
+        redeemedItems_atAddress[redeemFrom] += 1; // Increment redeemed items count
+        redeemedItemNames_atAddress[redeemFrom].push(itemName); // Add redeemed item name
+        emit Redeem(redeemFrom, redeemValue, itemName);
+        return redeemedItems_atAddress[redeemFrom];  // Returning total redeemed items count
+    }
+
+    function getRedeemedItemNames(address account) public view returns (string[] memory) {
+        require(isExist[account], "Account doesn't exist.");
+        return redeemedItemNames_atAddress[account];
     }
 
     function totalSupply() public view returns (uint) {
